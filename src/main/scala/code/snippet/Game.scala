8e2ae3e7e07aecfa4543e69b84ef4709a8db6cfc
Kristof Jozsa <kristof.jozsa@gmail.com>
2012-11-08 19:21:00 +0100

    bootstrapped

--- src/main/scala/code/snippet/Game.scala
+++ src/main/scala/code/snippet/Game.scala
@@ -1,15 +1,17 @@
 package code.snippet
 
 import scala.math.random
-
 import net.liftweb.common.Full
 import net.liftweb.http.S
 import net.liftweb.http.SHtml
 import net.liftweb.http.StatefulSnippet
 import net.liftweb.util.Helpers.asInt
 import net.liftweb.util.Helpers.strToCssBindPromoter
+import net.liftweb.http.SessionVar
+import net.liftweb.common.Loggable
 
 class Game extends StatefulSnippet {
+  private val origin = "/numberguess"
   private val secret = 1 + random * 100 toInt
   private var guess = ""
   private var count = 0
@@ -23,13 +25,14 @@ class Game extends StatefulSnippet {
 
   def submit() = {
     println("matching " + guess + " on secret " + secret)
+
     asInt(guess) match {
       case Full(a) if a < secret => S.notice("try higher")
       case Full(a) if a > secret => S.notice("try lower")
-      case Full(a) if a == secret => S.notice("YOU WON! in " + count + " guesses"); S.redirectTo("/")
+      case Full(a) if a == secret => S.notice("YOU WON in " + count + " guesses!"); S.redirectTo(origin)
       case _ =>
     }
 
-    this.redirectTo("/")
+    this.redirectTo(origin)
   }
 }
