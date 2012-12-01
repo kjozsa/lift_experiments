3a75bf4daa5bd40fc5b874c780cdfb6e3fc7ce2e
Kristof Jozsa <kristof.jozsa@gmail.com>
2012-12-01 23:22:21 +0100

    simplified jettylauncher, made it jetty9-compatible

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,6 +1,6 @@
 import org.eclipse.jetty.server.Server
+
 import org.eclipse.jetty.servlet.{ DefaultServlet, ServletContextHandler }
-import org.eclipse.jetty.server.nio.SelectChannelConnector
 import net.liftweb.http.LiftFilter
 import java.util.EnumSet
 import javax.servlet.DispatcherType
@@ -8,10 +8,7 @@ import org.eclipse.jetty.webapp.WebAppContext
 
 object JettyLauncher extends App {
   val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
-  val server = new Server
-  val scc = new SelectChannelConnector
-  scc.setPort(port)
-  server.setConnectors(Array(scc))
+  val server = new Server(port)
 
   val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
   context.addServlet(classOf[DefaultServlet], "/");
