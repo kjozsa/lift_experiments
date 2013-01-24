66af08a9f6c716c1b52cba93e201c71e634c5a55
Kristof Jozsa <kristof.jozsa@gmail.com>
2013-01-25 00:38:31 +0100

    typed select box

--- src/main/scala/code/snippet/ScreenForm.scala
+++ src/main/scala/code/snippet/ScreenForm.scala
@@ -4,11 +4,18 @@ import net.liftweb.http.LiftScreen
 import net.liftweb.http.S
 
 object ScreenForm extends LiftScreen {
-
   val name = field("Name", "")
   val age: Int = field("Age", 0, minVal(13, "Too young"))
 
+  abstract sealed class Gender
+
+  case object Male extends Gender
+
+  case object Female extends Gender
+
+  val gender = select("Gender", Male, Seq(Male, Female))
+
   def finish() {
-    S.notice("Form submitted")
+    S.notice("Form submitted, chosen gender: " + gender)
   }
 }
