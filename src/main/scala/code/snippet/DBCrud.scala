3e824b4a179917f5e2e01cdb1923289aad0d52fd
Kristof Jozsa <kristof.jozsa@gmail.com>
2013-02-07 13:10:44 +0100

    simplified with PassThru

--- src/main/scala/code/snippet/DBCrud.scala
+++ src/main/scala/code/snippet/DBCrud.scala
@@ -3,7 +3,6 @@ package code.snippet
 import net.liftweb._
 import http.{PaginatorSnippet, SHtml}
 import util._
-import scala.xml.NodeSeq
 import Helpers._
 import net.liftweb.http.SHtml._
 import code.model.Company
@@ -18,11 +17,11 @@ object DBCrud extends PaginatorSnippet[Company] {
     SHtml.idMemoize(outer =>
       ClearClearable &
 
-        ".hasRecords" #> ((ns: NodeSeq) => if (count == 0) NodeSeq.Empty else ns) &
-
         ".companies *" #> page.map(company =>
           ".company-name *" #> company.name.toString
         ) &
+
+        ".hasRecords" #> (if (count == 0) ClearNodes else PassThru) &
 
         ".createRecord [onclick]" #> ajaxInvoke(() => {
           val ftl = Company.createRecord.name("FTL Development " + randomInt(100))
